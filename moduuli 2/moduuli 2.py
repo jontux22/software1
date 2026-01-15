c99ae863b11c5e264d9a3fa7a46984dc960e8eb5
jontux22 <koutelojoonas22@gmail.com>
2026-01-15 10:49:40 +0200

    first commit

---
+++ moduuli 2/moduuli 2.py
@@ -0,0 +1,24 @@
+# pisteet = 50  # Muuttujan pisteet arvo on nyt 50
+# print(pisteet)  # Tulostaa: 50
+#
+# pisteet = 120  # Nyt muuttujan pisteet arvo on 120
+# print(pisteet)  # Tulostaa: 120
+
+# eka = -9
+# toka = 12_456_123_180
+# kolmas = 4.973
+# neljäs = -4 + 2j
+#
+# print(eka)
+# print(toka)
+# print(kolmas)
+# print(neljäs)
+# print(neljäs.real)
+# print(neljäs.imag)
+
+print(20-5)
+print(20*252)
+print(20/5)
+print(20**5)
+print(200%2)
+
