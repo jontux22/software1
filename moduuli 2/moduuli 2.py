48358af0951b067d862f4e5d071e42efe019e520
jontux22 <koutelojoonas22@gmail.com>
2026-01-15 13:53:30 +0200

    muutoksia

--- moduuli 2/moduuli 2.py
+++ moduuli 2/moduuli 2.py
@@ -1,24 +1,4 @@
-# pisteet = 50  # Muuttujan pisteet arvo on nyt 50
-# print(pisteet)  # Tulostaa: 50
-#
-# pisteet = 120  # Nyt muuttujan pisteet arvo on 120
-# print(pisteet)  # Tulostaa: 120
-
-# eka = -9
-# toka = 12_456_123_180
-# kolmas = 4.973
-# neljäs = -4 + 2j
-#
-# print(eka)
-# print(toka)
-# print(kolmas)
-# print(neljäs)
-# print(neljäs.real)
-# print(neljäs.imag)
-
-print(20-5)
-print(20*252)
-print(20/5)
-print(20**5)
-print(200%2)
+# name =input("Give name: ")
+# greeting= f"Hello, {name}!"
+# print
 
